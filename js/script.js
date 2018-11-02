e8b766e0e43ff5470b9d03a005996830477cc54b
tiangela <tas14@yandex.ru>
2018-11-02 09:07:53 +0300

    Адаптив

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-$(document).ready(function() {
 
-	$('.main__img-zoom').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
-		}
+// Меню навигации
+	var navMain = document.querySelector('.menu');
+	var navToggle = document.querySelector('.menu__toggle');
+	var scroll = document.querySelector('.scroll');
+
+	navMain.classList.remove('menu--nojs');
+	//scroll.classList.remove('scroll--js');
+
+	navToggle.addEventListener('click', function() {
+	  if (navMain.classList.contains('menu--closed')) {
+	    navMain.classList.remove('menu--closed');
+	    navMain.classList.add('menu--open');
+	  //  scroll.classList.add('scroll--js');
+
+	  } else {
+	    navMain.classList.add('menu--closed');
+	    navMain.classList.remove('menu--open');
+	//    scroll.classList.remove('scroll--js');
+	  }
 	});
-  });
+
+	$(document).ready(function() {
+
+		$('.main__img-zoom').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			image: {
+				verticalFit: false
+			}
+		});
+	  });
